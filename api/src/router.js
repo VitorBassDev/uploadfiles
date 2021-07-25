48ea6c04fc20ca59a2a8ac1363861daa44fa7c1a
Vítor Guedes <vittorbassdev@gmail.com>
2021-07-24 21:37:04 -0300

    Refactory: Rota Incial da aplicaçao Ajuste da rota inicial da aplicaçao

--- api/src/router.js
+++ api/src/router.js
@@ -2,12 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   return res
   .status(200)
-  .json({
-    messagem: "Página inicial do projeto"
-  })
+  .json("Página inicial do projeto")
 })
 
 module.exports = router
